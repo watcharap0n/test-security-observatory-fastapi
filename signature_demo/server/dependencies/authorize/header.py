59e72388b003751c626c50bb767cbc0301aa2b78
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2022-12-14 15:54:27 +0700

    add token header.py

---
+++ signature_demo/server/dependencies/authorize/header.py
@@ -0,0 +1,10 @@
+import os
+from typing import Union
+from fastapi import Header, status, HTTPException
+
+
+async def signature_jwt_header(jwt_header: Union[str, None] = Header(default=None)):
+    if not jwt_header:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='JWT header is bad request.')
+    if jwt_header != os.getenv('JWT_HEADER', None):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='JWT header invalid')
