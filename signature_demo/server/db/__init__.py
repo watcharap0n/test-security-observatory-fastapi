b61651150fb1c4af0b8a693bbd989a053888015b
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2022-12-14 15:52:58 +0700

    add constructor in object oriented

---
+++ signature_demo/server/db/__init__.py
@@ -0,0 +1,34 @@
+"""
+database and authentication master
+    - mongodb
+    - firebase realtime
+    - firebase admin (authentication)
+
+"""
+
+import os
+from .database import MongoDB
+from .object_str import CutId, PyObjectId
+from pymongo import MongoClient
+
+client = MongoClient(
+    host=os.getenv('MONGODB_URI', 'mongodb://localhost:27018'),
+    username=os.getenv('MONGO_INITDB_USERNAME', None),
+    password=os.getenv('MONGO_INITDB_PASSWORD', None)
+)
+
+db = MongoDB(
+    database_name="SignatureDemo",
+    client=client
+)
+
+
+def generate_token(engine):
+    """
+
+    :param engine:
+    :return:
+    """
+    obj = CutId(_id=engine)
+    Id = obj.dict()["id"]
+    return Id
