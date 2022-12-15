e90e39074bcb31347e74d1736182bcd01f707936
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2022-12-15 15:52:58 +0700

    update model structure initialize.py

--- signature_middleware/server/models/initialize.py
+++ signature_middleware/server/models/initialize.py
@@ -1,80 +1,9 @@
+import pytz
+from datetime import datetime
 from bson import ObjectId
 from ..db import PyObjectId
 from typing import Optional, Union, List
-from pydantic import BaseModel, Field
-
-
-class CertificateJDS(BaseModel):
-    signerProfileName: Optional[str] = Field(
-        'yourSign_test_itmd',
-        regex='^(?![0-9._])(?!.*[._]$)(?!.*\d_)(?!.*_\d)[a-zA-Z0-9_]+$',
-        description='Allow only alphabetic eng character & number endswith.'
-    )
-    signerPassword: Optional[str] = 'P@ssw0rd'
-    signerPurpose: Optional[str] = 'GENERAL'
-    profileName: str
-    password: str
-    commonName: Optional[str] = 'demo signs'
-    orgUnit: Optional[str] = 'Graduate Studies'
-    org: Optional[str] = 'Vidyasirimedhi Institute of Science and Technology'
-    locality: Optional[str] = 'Wangchan'
-    state: Optional[str] = 'Rayoug'
-    country: Optional[str] = 'TH'
-    validityDays: Optional[int] = 730
-    reqRefNo: Union[int, None] = None
-
-    class Config:
-        validate_assigment = True
-        schema_extra = {
-            'example': {
-                'signerProfileName': 'yourSign_test_itmd',
-                'signerPassword': 'P@ssw0rd',
-                'signerPurpose': 'GENERAL',
-                'profileName': 'Arak',
-                'password': 'secret',
-                'commonName': 'demo signs',
-                'orgUnit': 'Graduate Studies',
-                'org': 'Vidyasirimedhi Institute of Science and Technology',
-                'locality': 'Wangchan',
-                'state': 'Rayoug',
-                'country': 'TH',
-                'validityDays': 730
-            }
-        }
-
-
-class Profile(BaseModel):
-    name: str
-    available_people: Union[List, None] = []
-    detail: CertificateJDS
-
-
-class Terminal(BaseModel):
-    name: Union[str, None] = None
-    owner: str
-    certs: Union[List, None] = []
-
-
-class Intermediate(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
-    type: str
-    name: str = Field(
-        ...,
-        regex='^(?![0-9._])(?!.*[._]$)(?!.*\d_)(?!.*_\d)[a-zA-Z0-9 ]+$',
-        description='Allow only alphabetic eng character & number endswith.'
-    )
-    terminal: Union[List, None] = []
-
-    class Config:
-        json_encoders = {ObjectId: str}
-        validate_assignment = True
-        schema_extra = {
-            'example': {
-                'type': 'personal',
-                'name': 'watcharapon',
-                'terminal': []
-            }
-        }
+from pydantic import BaseModel, Field, validator
 
 
 class Initialized(BaseModel):
@@ -87,6 +16,7 @@ class Initialized(BaseModel):
     quota: Optional[int] = 100
     remain_quota: Optional[int] = 100
     intermediate: Union[List, None] = []
+    date: Optional[datetime] = None
 
     class Config:
         json_encoders = {ObjectId: str}
@@ -100,23 +30,31 @@ class Initialized(BaseModel):
             }
         }
 
+    @validator('date', pre=True, always=True)
+    def set_date(cls, date):
+        tz = pytz.timezone('Asia/Bangkok')
+        dt = datetime.now(tz)
+        return dt
 
-class UpdateIntermediate(BaseModel):
-    type: str
-    name: str = Field(
+
+class UpdateInitialize(BaseModel):
+    company_name: str = Field(
         ...,
-        regex='^(?![0-9._])(?!.*[._]$)(?!.*\d_)(?!.*_\d)[a-zA-Z0-9 ]+$',
+        regex='^(?![0-9._])(?!.*[._]$)(?!.*\d_)(?!.*_\d)[a-zA-Z0-9]+$',
         description='Allow only alphabetic eng character & number endswith.'
     )
-    terminal: Union[List, None] = []
+    quota: Optional[int] = 100
+    remain_quota: Optional[int] = 100
+    intermediate: Union[List, None] = []
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
             'example': {
-                'type': 'personal',
-                'name': 'watcharapon weeraborirak',
-                'terminal': []
+                'company_name': 'personal',
+                'quota': 100,
+                'remain_quota': 99,
+                'intermediate': []
             }
         }
