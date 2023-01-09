85a9e1188341ba60f9f5a2a29d0c1695d52aa394
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-01-09 11:32:17 +0700

    update header.py

--- signature_middleware/server/dependencies/authorize/header.py
+++ signature_middleware/server/dependencies/authorize/header.py
@@ -4,7 +4,6 @@ from fastapi import Header, status, HTTPException
 
 
 async def signature_jwt_header(jwt_header: Union[str, None] = Header(default=None)):
-    print(jwt_header)
     if not jwt_header:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='JWT header is bad request.')
     if jwt_header != os.getenv('JWT_HEADER', None):
