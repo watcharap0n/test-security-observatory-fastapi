8411bf28e82e2ae68a9055bff03f0f41462c1c1a
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-01-14 23:45:09 +0700

    update permissions evaluate.py

--- signature_middleware/server/dependencies/router/evaluate.py
+++ signature_middleware/server/dependencies/router/evaluate.py
@@ -2,6 +2,7 @@ from fastapi import status, HTTPException, Path, Depends
 from ...db import db
 from ...authentication import get_signs_active_user
 from ...models.initialize import Initialized
+from ...models.terminal import Terminal
 from ...models.intermediate import UpdateIntermediate, Intermediate
 from ...models.authentication import User
 
@@ -86,16 +87,24 @@ async def evaluate_duplication_organization(
     validate_name_org = list(validate_name_org)
     if len(validate_name_org) > 1:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail='Company name is duplicate.')
+                            detail='Issued company duplicate.')
     return payload
 
 
-async def permission_access_terminal_cert(
-        id: str = Path(description='ID document terminal cert'),
+async def admin_via_create_terminal(
+        payload: Terminal,
         current_user: User = Depends(get_signs_active_user)
 ):
-    if (await db.find_one(collection='terminals',
-                          query={'owner.uid': current_user.uid})) is None:
+    if current_user.role == 'Member':
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Not enough to access denies')
+                            detail='Not enough to permission.')
+    return payload
+
+
+async def admin_via_fd_terminal(
+        current_user: User = Depends(get_signs_active_user)
+):
+    if current_user.role == 'Member':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Not enough to permission.')
     return id
